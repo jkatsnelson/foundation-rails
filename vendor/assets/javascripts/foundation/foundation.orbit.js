dcac1aaa65db72c2c622af01008e3a142645b67e
John Katsnelson <voltalocke@gmail.com>
2014-04-30 12:26:36 -0700

    hotfixes mobile scroll on orbit

--- vendor/assets/javascripts/foundation/foundation.orbit.js
+++ vendor/assets/javascripts/foundation/foundation.orbit.js
@@ -50,7 +50,7 @@
       container = slides_container.parent();
       slides_container.addClass(settings.slides_container_class);
       slides_container.addClass(settings.animation);
-      
+
       if (settings.stack_on_small) {
         container.addClass(settings.stack_on_small_class);
       }
@@ -92,15 +92,15 @@
     self._prepare_direction = function(next_idx, current_direction) {
       var dir = 'next';
       if (next_idx <= idx) { dir = 'prev'; }
-      
-      if (settings.animation === 'slide') {    
+
+      if (settings.animation === 'slide') {
         setTimeout(function(){
           slides_container.removeClass("swipe-prev swipe-next");
           if (dir === 'next') {slides_container.addClass("swipe-next");}
           else if (dir === 'prev') {slides_container.addClass("swipe-prev");}
         },0);
       }
-      
+
       var slides = self.slides();
       if (next_idx >= slides.length) {
         if (!settings.circular) return false;
@@ -111,7 +111,7 @@
       }
       var current = $(slides.get(idx))
         , next = $(slides.get(next_idx));
-      
+
       return [dir, current, next, next_idx];
     };
 
@@ -120,7 +120,7 @@
       if (self.cache.animating) {return false;}
       if (next_idx === idx) {return false;}
       if (typeof self.cache.timer === 'object') {self.cache.timer.restart();}
-      
+
       var slides = self.slides();
       self.cache.animating = true;
       var res = self._prepare_direction(next_idx)
@@ -138,7 +138,7 @@
 
       current.css("transitionDuration", settings.animation_speed+"ms");
       next.css("transitionDuration", settings.animation_speed+"ms");
-      
+
       var callback = function() {
         var unlock = function() {
           if (start_timer === true) {self.cache.timer.restart();}
@@ -152,7 +152,7 @@
           setTimeout(function(){
             self.cache.animating = false;
           }, 100);
-          
+
         };
         if (slides_container.height() != next.height() && settings.variable_height) {
           slides_container.animate({'min-height': next.height()}, 250, 'linear', unlock);
@@ -165,7 +165,7 @@
 
       var start_animation = function() {
         if (dir === 'next') {animate.next(current, next, callback);}
-        if (dir === 'prev') {animate.prev(current, next, callback);}        
+        if (dir === 'prev') {animate.prev(current, next, callback);}
       };
 
       if (next.height() > slides_container.height() && settings.variable_height) {
@@ -174,7 +174,7 @@
         start_animation();
       }
     };
-    
+
     self.next = function(e) {
       e.stopImmediatePropagation();
       e.preventDefault();
@@ -183,7 +183,7 @@
         self._goto(idx + 1);
     }, 100);
     };
-    
+
     self.prev = function(e) {
       e.stopImmediatePropagation();
       e.preventDefault();
@@ -207,7 +207,7 @@
     };
 
     // Click handler for slides and bullets.
-    self.link_bullet = function(e) {    
+    self.link_bullet = function(e) {
       var index = $(this).attr('data-orbit-slide');
       if ((typeof index === 'string') && (index = $.trim(index)) != "") {
         if(isNaN(parseInt(index)))
@@ -235,7 +235,7 @@
     self.timer_callback = function() {
       self._goto(idx + 1, true);
     }
-    
+
     self.compute_dimensions = function() {
       var current = $(self.slides().get(idx));
       var h = current.height();
@@ -249,8 +249,8 @@
 
     self.create_timer = function() {
       var t = new Timer(
-        container.find('.'+settings.timer_container_class), 
-        settings, 
+        container.find('.'+settings.timer_container_class),
+        settings,
         self.timer_callback
       );
       return t;
@@ -264,7 +264,7 @@
       var t = container.find('.'+settings.timer_container_class);
       if (t.hasClass(settings.timer_paused_class)) {
         if (typeof self.cache.timer === 'undefined') {self.cache.timer = self.create_timer();}
-        self.cache.timer.start();     
+        self.cache.timer.start();
       }
       else {
         if (typeof self.cache.timer === 'object') {self.cache.timer.stop();}
@@ -274,10 +274,10 @@
     self.init = function() {
       self.build_markup();
       if (settings.timer) {
-        self.cache.timer = self.create_timer(); 
+        self.cache.timer = self.create_timer();
         Foundation.utils.image_loaded(this.slides().children('img'), self.cache.timer.start);
       }
-      
+
       animate = new CSSAnimation(settings, slides_container);
 
       if (has_init_active) {
@@ -295,13 +295,12 @@
       if (settings.next_on_click) {
         container.on('click', '.'+settings.slides_container_class+' [data-orbit-slide]', self.link_bullet);
       }
-      
+
       container.on('click', self.toggle_timer);
       if (settings.swipe) {
         slides_container.on('touchstart.fndtn.orbit',function(e) {
           if (self.cache.animating) {return;}
           if (!e.touches) {e = e.originalEvent;}
-          e.preventDefault();
           e.stopPropagation();
 
           self.cache.start_page_x = e.touches[0].pageX;
@@ -310,16 +309,16 @@
           self.cache.delta_x = 0;
           self.cache.is_scrolling = null;
           self.cache.direction = null;
-          
-          self.stop_timer(); // does not appear to prevent callback from occurring          
+
+          self.stop_timer(); // does not appear to prevent callback from occurring
         })
         .on('touchmove.fndtn.orbit',function(e) {
           if (Math.abs(self.cache.delta_x) > 5) {
             e.preventDefault();
-            e.stopPropagation();
+            if(!self.cache.is_scrolling) e.stopPropagation();
           }
 
-          if (self.cache.animating) {return;}          
+          if (self.cache.animating) {return;}
           requestAnimationFrame(function(){
             if (!e.touches) { e = e.originalEvent; }
 
@@ -335,7 +334,7 @@
             if (self.cache.is_scrolling) {
               return;
             }
-            
+
             var direction = (self.cache.delta_x < 0) ? (idx+1) : (idx-1);
             if (self.cache.direction !== direction) {
               var res = self._prepare_direction(direction);
@@ -347,7 +346,7 @@
 
             if (settings.animation === 'slide') {
               var offset, next_offset;
-              
+
               offset = (self.cache.delta_x / container.width()) * 100;
               if (offset >= 0) {next_offset = -(100 - offset);}
               else {next_offset = 100 + offset;}
@@ -360,7 +359,7 @@
         .on('touchend.fndtn.orbit', function(e) {
           if (self.cache.animating) {return;}
           e.preventDefault();
-          e.stopPropagation();
+          if(!self.cache.is_scrolling) e.stopPropagation();
           setTimeout(function(){
             self._goto(self.cache.direction);
           }, 50);
@@ -376,7 +375,7 @@
           self.cache.timer.start();
         }
       });
-      
+
       $(document).on('click', '[data-orbit-link]', self.link_custom);
       $(window).on('load resize', self.compute_dimensions);
       var children = this.slides().find('img');
@@ -397,7 +396,7 @@
         duration = settings.timer_speed,
         progress = el.find('.'+settings.timer_progress_class),
         do_progress = progress && progress.css('display') != 'none',
-        start, 
+        start,
         timeout,
         left = -1;
 
@@ -606,5 +605,5 @@
     }
   };
 
-    
+
 }(jQuery, window, window.document));
